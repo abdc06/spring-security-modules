10f05634c05e72a5c11eed633793c84409521353
abdc06 <abdc2806@gmail.com>
2022-11-08 21:37:59 +0900

    web-rest-basic-auth 프로젝트 생성

---
+++ web-rest-basic-auth/src/main/java/me/abdc/webrestbasicauth/WebRestBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package me.abdc.webrestbasicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRestBasicAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRestBasicAuthApplication.class, args);
+    }
+
+}
